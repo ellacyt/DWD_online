11b4d19e071e1b34dda89b1ce4981cd0c32f840b
Ella Chung <ellacyt@gmail.com>
2018-02-01 14:31:43 -0500

    added form.html

--- Week2/webserver.js
+++ Week2/webserver.js
@@ -1,16 +1,20 @@
 var express = require('express')
 var app = express()
 
+app.use(express.statistic('public'));
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
+var count = 0;
+
+app.get('/somethingelse', function (req, res) {
+  res.send('<html><body><h1>Something Else' + count + '</h1></body></html>');
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 http://IP:3000/
 Of course a route can be for any "path", here is an example for "/somethingelse":
-
-app.get('/somethingelse', function (req, res) {
-  res.send('Hello World!');
-});
