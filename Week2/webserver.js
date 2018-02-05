93e4bd425bfa0b8703847707f96d4e0045463e17
Ella Chung <ellacyt@gmail.com>
2018-02-04 23:02:50 -0500

    added form

--- Week2/webserver.js
+++ Week2/webserver.js
@@ -13,8 +13,13 @@ app.get('/somethingelse', function (req, res) {
   res.send('<html><body><h1>Something Else' + count + '</h1></body></html>');
 });
 
+app.get('/formpost', function (req, res) {
+  console.log("They submitted:" + req.query.textfield);
+  res.send("They submitted:" + req.query.textfield);
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
-http://IP:3000/
-Of course a route can be for any "path", here is an example for "/somethingelse":
+//http://IP:3000/
+//Of course a route can be for any "path", here is an example for "/somethingelse":
